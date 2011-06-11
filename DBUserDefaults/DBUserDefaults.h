0a3cb773f497ddca81479e73679cf5c1a2803178
Tyler Bunnell <tyler@mizage.com>
2011-06-10 22:46:55 -0600

    Add initial project and files.

---
+++ DBUserDefaults/DBUserDefaults.h
@@ -0,0 +1,16 @@
+//
+//  DBUserDefaults.h
+//  DBUserDefaults
+//
+//  Created by Tyler Bunnell on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DBUserDefaults : NSObject {
+    
+}
+
+@end
---
+++ DBUserDefaults/DBUserDefaults.m
@@ -0,0 +1,14 @@
+//
+//  DBUserDefaults.m
+//  DBUserDefaults
+//
+//  Created by Tyler Bunnell on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DBUserDefaults.h"
+
+
+@implementation DBUserDefaults
+
+@end
