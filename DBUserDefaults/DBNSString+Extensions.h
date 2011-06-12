ea8537b1bf52ac8ba9962a47dc63ad156010c1da
Tyler Bunnell <tyler@mizage.com>
2011-06-11 22:12:17 -0600

    Add DBUtils.
    Add methods to detect Dropbox exists and where the folder is.

---
+++ DBUserDefaults/DBNSString+Extensions.h
@@ -0,0 +1,16 @@
+//
+//  DBNSString+Extensions.h
+//  DBUserDefaults
+//
+//  Created by Tyler Bunnell on 6/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DBNSString_Extensions : NSString {
+    
+}
+
+@end
---
+++ DBUserDefaults/DBNSString+Extensions.m
@@ -0,0 +1,14 @@
+//
+//  DBNSString+Extensions.m
+//  DBUserDefaults
+//
+//  Created by Tyler Bunnell on 6/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DBNSString+Extensions.h"
+
+
+@implementation DBNSString_Extensions
+
+@end
